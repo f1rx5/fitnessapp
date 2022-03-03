f08003f84d0f61891570390a5bc9d494a9e85318
firas shareef <firasshareef30@gmail.com>
2022-03-03 15:14:09 +0530

    ver2

--- app1/urls.py
+++ app1/urls.py
@@ -6,4 +6,10 @@ urlpatterns=[
     path('about/',views.fnabout,name='about'),
     path('classes/',views.fnclasses,name='classes'),
     path('register/',views.fnregister,name='register'),
+    path('master2/',views.fnmaster2,name='master2'),
+    path('user/',views.fnuser,name='user'),
+    path('usermaster/',views.fnmaster3,name='usermaster'),
+    path('viewworkout/',views.fnworkout,name='viewworkout'),
+    path('viewdiet/',views.fndiet,name='viewdiet'),
+    path('contact/',views.fncontact,name='contact'),
 ]
--- app1/views.py
+++ app1/views.py
@@ -11,3 +11,16 @@ def fnclasses(request):
     return render(request,'classes.html')
 def fnregister(request):
     return render(request,'register.html')
+def fnmaster2(request):
+    return render(request,'master2.html')
+def fnuser(request):
+    return render(request,'userprofile.html')
+def fnmaster3(request):
+    return render(request,'usermaster.html')
+def fnworkout(request):
+    return render(request,'viewworkout.html')
+def fndiet(request):
+    return render(request,'viewdiet.html')
+def fncontact(request):
+    return render(request,'contact.html')
+
